26f919dd8816667e9c04c0b3be4f22645b13548b
DavidCogollo <davidcogolloperez@gmail.com>
2022-02-17 09:53:22 +0100

    Primer Commit

---
+++ apppwebmvc/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO empleados(nombre,apellido,email,telefono) VALUES ("Daniel","Garcia","d@gmail","64567959");
+INSERT INTO empleados(nombre,apellido,email,telefono) VALUES ("Marta","Rodrigez","m@gmail","634567890");
+INSERT INTO empleados(nombre,apellido,email,telefono) VALUES ("Guillermo","Blazquez","g@gmail","234098654");
+INSERT INTO empleados(nombre,apellido,email,telefono) VALUES ("Sergio","Peguero","s@gmail","3456789876");
+INSERT INTO empleados(nombre,apellido,email,telefono) VALUES ("Paula","Cano","p@gmail.com","456789090");
